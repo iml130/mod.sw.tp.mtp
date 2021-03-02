b6a9a97f3c6a598f01e025a4243a7ebe0ec63daf
Dennis Luensch <dennis.luensch@iml.fraunhofer.de>
2021-03-02 14:40:02 +0100

    Add IML license text to source and header files

--- mars_common/include/mars_common/exception/SetParamException.h
+++ mars_common/include/mars_common/exception/SetParamException.h
@@ -1,16 +1,18 @@
-/* ------------------------------------------------------------------------------------------------
- * Fraunhofer IML
- * Department Automation and Embedded Systems
- * ------------------------------------------------------------------------------------------------
- * project          : mars_agent_robot
- * ------------------------------------------------------------------------------------------------
- * Author(s)        : Dennis Luensch
- * Contact(s)       : dennis.luensch@iml.fraunhofer.de
- * ------------------------------------------------------------------------------------------------
- * Tabsize          : 2
- * Charset          : UTF-8
- * ---------------------------------------------------------------------------------------------
- */
+//  Copyright 2020 Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  https:www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
 
 #ifndef MARS_COMMON_SETPARAMEXCEPTION_H
 #define MARS_COMMON_SETPARAMEXCEPTION_H
--- mars_common/src/behavior/BlackboardAccessElementManager.cpp
+++ mars_common/src/behavior/BlackboardAccessElementManager.cpp
@@ -1,15 +1,18 @@
-/* ------------------------------------------------------------------------------------------------
- * Fraunhofer IML
- * Department Automation and Embedded Systems
- * ------------------------------------------------------------------------------------------------
- * project          : MARS
- * ------------------------------------------------------------------------------------------------
- * Author(s)        : Dennis Luensch
- * Contact(s)       : dennis.luensch@iml.fraunhofer.de
- * ------------------------------------------------------------------------------------------------
- * Tabsize          : 2
- * Charset          : UTF-8
- * --------------------------------------------------------------------------------------------- */
+//  Copyright 2020 Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  https:www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
 
 #include "mars_common/behavior/BlackboardAccessElementManager.h"
 #include "mars_common/Logger.h"
